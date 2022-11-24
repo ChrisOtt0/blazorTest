f93a33c6f2808b579f1f02d1c3115eda15d8ac29
riccardigithub <riccardiroca@gmail.com>
2022-11-24 19:57:36 +0100

    updates
    Signed-off-by: riccardihithub <101105168+RiccardiGitHub@users.noreply.github.com>

--- Server/Controllers/SuperHeroController.cs
+++ Server/Controllers/SuperHeroController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blazorTest.Server.Controllers
@@ -11,17 +6,28 @@ namespace blazorTest.Server.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        public static List<Comic> comics = new List<Comic>
+        private static List<Comic> comics = new List<Comic>
         {
             new Comic() {Id = 1, Name = "Marvel"},
-            new Comic() {Id = 1, Name = "DCS"},
-            new Comic() {Id = 1, Name = "Melanie"},
+            new Comic() {Id = 2, Name = "DCS"},
+            new Comic() {Id = 3, Name = "Melanie"},
         };
-        public static List<SuperHero> heroes = new List<SuperHero>
+
+        private static List<SuperHero> heroes = new List<SuperHero>
         {
             new SuperHero() {Id = 1, FirstName = "Luca", LastName = "Riccardi", HeroName = "Your man", Comic = comics[0]},
-            new SuperHero() {Id = 1, FirstName = "DCS"},
-            new SuperHero() {Id = 1, FirstName = "Melanie"},
+            new SuperHero() {Id = 2, FirstName = "DCS", LastName = "Sophus", HeroName = "10xProgrammer", Comic = comics[1]},
+            new SuperHero() {Id = 3, FirstName = "Melanie", LastName = "Nielson", HeroName = "Blond girl", Comic = comics[2]}
         };
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return Ok(heroes);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetSuperHero(int id)
+        {
+            return Ok(heroes.FirstOrDefault(x => x.Id == id));
+        }
     }
 }
